7d2691ded027596ed17a797479c475f91417e0fb
David Morrow <quickbasic22@yahoo.com>
2020-03-21 20:24:56 -0400

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace SpeechReco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnSpeechStartup = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(288, 81);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Status";
+            // 
+            // btnSpeechStartup
+            // 
+            this.btnSpeechStartup.Location = new System.Drawing.Point(187, 582);
+            this.btnSpeechStartup.Name = "btnSpeechStartup";
+            this.btnSpeechStartup.Size = new System.Drawing.Size(138, 46);
+            this.btnSpeechStartup.TabIndex = 1;
+            this.btnSpeechStartup.Text = "Startup Speech Recognition";
+            this.btnSpeechStartup.UseVisualStyleBackColor = true;
+            this.btnSpeechStartup.Click += new System.EventHandler(this.btnSpeechStartup_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 197);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(246, 368);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(264, 197);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(240, 368);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(191, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 640);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSpeechStartup);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnSpeechStartup;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Drawing.Drawing2D;
+
+namespace SpeechReco
+{
+    public partial class Form1 : Form
+    {
+        System.Speech.Recognition.SpeechRecognitionEngine speech;
+        System.Speech.Synthesis.SpeechSynthesizer synthesizer;
+        System.Array colorsArray;
+        KnownColor[] allColors;
+        Choices colors;
+        Point drawingPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            getColors();
+        }
+
+        private void getColors()
+        {
+            colorsArray = Enum.GetValues(typeof(KnownColor));
+            allColors = new KnownColor[colorsArray.Length];
+            colors = new Choices();
+        }
+
+        private void SpeechStartup()
+        {
+            var CultureInfo = new System.Globalization.CultureInfo("en-US");
+            speech = new SpeechRecognitionEngine(CultureInfo);
+
+            Choices colors = new Choices();
+            foreach (KnownColor item in colorsArray)
+            {
+                colors.Add(new string[] { item.ToString() });
+            }
+            
+
+            speech.SetInputToDefaultAudioDevice();
+
+            GrammarBuilder gb = new GrammarBuilder();
+           
+            gb.Append(colors);
+
+            Grammar g = new Grammar(gb);
+
+            speech.LoadGrammar(g);
+
+            speech.SpeechRecognized +=
+        new EventHandler<SpeechRecognizedEventArgs>(speech_SpeechRecognized);
+
+            speech.SpeechRecognitionRejected += Speech_SpeechRecognitionRejected;
+
+            speech.RecognizeAsync(RecognizeMode.Multiple);
+
+        }
+
+        private void Speech_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            foreach (var item in e.Result.Alternates.ToList())
+            {
+                lblStatus.Text += item.Homophones.ToString();
+            }
+        }
+
+        private void speech_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            int colorFailure = 0;
+            
+            Array.Copy(colorsArray, allColors, colorsArray.Length);
+            listBox2.Items.Add(e.Result.Text);
+            foreach(KnownColor kc in allColors)
+            {
+                if (kc.ToString().ToLower() == e.Result.Text.ToLower())
+                {
+                    try
+                    {
+                        Form1.ActiveForm.BackColor = Color.FromName(e.Result.Text.ToLowerInvariant());
+                    }
+                    catch (Exception ex)
+                    {
+                        lblStatus.Text += ex.ToString();
+                    }
+                   
+                }
+                listBox1.Items.Add(kc);
+                
+                
+            }
+            
+        }
+
+        private void btnSpeechStartup_Click(object sender, EventArgs e)
+        {
+            SpeechStartup();
+            btnSpeechStartup.BackColor = Color.Green;
+
+            if (btnSpeechStartup.BackColor == Color.Green)
+            {
+                btnSpeechStartup.BackColor = Color.Blue;
+            }
+           
+                
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            Pen p = new Pen(Color.Fuchsia);
+
+            Point[] points = new Point[]
+            {
+                new Point(100, 150),
+                new Point(100, 150),
+                new Point(100, 150),
+                new Point(100, 150),
+                new Point(100, 150),
+                new Point(100, 150),
+                new Point(100, 150),
+                new Point(100, 150),
+                new Point(100, 150),
+                new Point(100, 150)
+            };
+
+            if (drawingPoint != null)
+            {
+                g.DrawPie(p, drawingPoint.X, drawingPoint.Y, 50, 50, 0, 360);
+            }
+
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = e.X.ToString();
+            textBox2.Text = e.Y.ToString();
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics g = Form1.ActiveForm.CreateGraphics();
+            Pen pen = new Pen(Color.Blue);
+            pen.Width = 15;
+
+
+            g.FillPie(Brushes.Violet, e.X - 20, e.Y - 20, 20, 20, 0, 360);
+            
+            Point point1 = new Point(1050, 150);
+            Point point2 = new Point(1000, 400);
+            g.DrawLine(pen, point1, point2);
+
+            Point point3 = new Point(980, 171);
+            Point point4 = new Point(845, 382);
+            g.DrawLine(pen, point1, point2);
+
+
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            synthesizer = new System.Speech.Synthesis.SpeechSynthesizer();
+            synthesizer.Speak(listBox1.SelectedItem.ToString());
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    listBox2.SelectedIndex++;
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.Text += ex.ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text += ex.ToString();
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    listBox2.SelectedIndex++;
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.Text += ex.ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text += ex.ToString();
+            }
+        }
+    }
+}
